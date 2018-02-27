2dbc7c2cdb4cfe74a5517cbc700fcc503806f1d6
icesharp <icesharp@126.com>
2018-02-28 01:27:15 +0800

    init

---
+++ src/main/java/com/lzh/linemonitor/SimpleController.java
@@ -0,0 +1,21 @@
+package com.lzh.linemonitor;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/cxy")
+    public String hello() {
+        return "Greetings from 大腿!<br/><br/>Hello fufu怡~";
+    }
+
+}

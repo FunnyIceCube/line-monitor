ea444c2d67e308e7d8c4d09c32df50a06e7e97ad
luzhaohui <luzhaohui@didichuxing.com>
2018-11-20 22:36:14 +0800

    update .gitignore

--- src/main/java/com/lzh/linemonitor/dao/pojo/LineDO.java
+++ src/main/java/com/lzh/linemonitor/dao/pojo/LineDO.java
@@ -1,7 +1,89 @@
 package com.lzh.linemonitor.dao.pojo;
 
-import lombok.Data;
 
-@Data
+import java.util.Date;
+
 public class LineDO {
+
+  private long id;
+  private String poiStartName;
+  private String poiStartLoc;
+  private String poiEndName;
+  private String poiEndLoc;
+  private long ownerId;
+  private String ownerName;
+  private Date createTime;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getPoiStartName() {
+    return poiStartName;
+  }
+
+  public void setPoiStartName(String poiStartName) {
+    this.poiStartName = poiStartName;
+  }
+
+
+  public String getPoiStartLoc() {
+    return poiStartLoc;
+  }
+
+  public void setPoiStartLoc(String poiStartLoc) {
+    this.poiStartLoc = poiStartLoc;
+  }
+
+
+  public String getPoiEndName() {
+    return poiEndName;
+  }
+
+  public void setPoiEndName(String poiEndName) {
+    this.poiEndName = poiEndName;
+  }
+
+
+  public String getPoiEndLoc() {
+    return poiEndLoc;
+  }
+
+  public void setPoiEndLoc(String poiEndLoc) {
+    this.poiEndLoc = poiEndLoc;
+  }
+
+
+  public long getOwnerId() {
+    return ownerId;
+  }
+
+  public void setOwnerId(long ownerId) {
+    this.ownerId = ownerId;
+  }
+
+
+  public String getOwnerName() {
+    return ownerName;
+  }
+
+  public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+  }
+
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
 }
--- src/main/java/com/lzh/linemonitor/dao/pojo/MonitorRecordDO.java
+++ src/main/java/com/lzh/linemonitor/dao/pojo/MonitorRecordDO.java
@@ -1,7 +1,49 @@
 package com.lzh.linemonitor.dao.pojo;
 
-import lombok.Data;
 
-@Data
+import java.util.Date;
+
 public class MonitorRecordDO {
+
+  private long id;
+  private long lineId;
+  private long duration;
+  private Date createTime;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getLineId() {
+    return lineId;
+  }
+
+  public void setLineId(long lineId) {
+    this.lineId = lineId;
+  }
+
+
+  public long getDuration() {
+    return duration;
+  }
+
+  public void setDuration(long duration) {
+    this.duration = duration;
+  }
+
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
 }
